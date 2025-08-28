2591db7b60d28343fec73949084b1f3c65b0a32e
kaushlendra singh <thakurkaushlendra32@gmail.com>
2025-08-28 21:58:18 +0530

    first commit

---
+++ multiplicationtable.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class multiplicationtable {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a positive integer (N): ");
+        int n = sc.nextInt();
+
+        if (n <= 0) {
+            System.out.println("Please enter a positive integer.");
+        } else {
+            System.out.println("Multiplication table of " + n + ":");
+            for (int i = 1; i <= 10; i++) {
+                System.out.println(n + " x " + i + " = " + (n * i));
+            }
+        }
+        sc.close();
+    }
+}
---
+++ sumnatural.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class sumnatural {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a positive integer (N): ");
+        int n = sc.nextInt();
+
+        if (n <= 0) {
+            System.out.println("Please enter a positive integer.");
+        } else {
+            int sum = n * (n + 1) / 2;
+            System.out.println("The sum of first " + n + " natural numbers is: " + sum);
+        }
+        sc.close();
+    }
+}
